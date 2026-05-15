3b99279d2ccc6dd72c3ec92caeb41c19b68b8b7a
yeejoeree <yeejo319@gmail.com>
2026-05-15 17:03:18 +0800

    footer update

--- src/set.js
+++ src/set.js
@@ -4,40 +4,127 @@ const footerTemplate = `
     <div class="footer-logo">
         <img src="static/logo.png" alt="INVASION PLUS">
     </div>
-    <div class="footer-content">
-        <div class="footer-column">
-            <h3>EXPLORE:</h3>
-            <ul>
-                <li><a href="allproduct.html">All Product</a></li>
-                <li><a href="cases.html">Cases</a></li>
-                <li><a href="cooling.html">Cooling</a></li>
-                <li><a href="monitor.html">Monitors</a></li>
-            </ul>
-        </div>                
-        <div class="footer-column">
-            <h3>ABOUT US:</h3>
-            <ul>
-                <li><a href="ContactUs.html">Contact Us</a></li>
-                <li><a href="Location.html">Location</a></li>
-            </ul>
-        </div>
-        <div class="footer-column">
-            <h3>LET'S CONNECT:</h3>
-            <div class="social-icons">
-                <a href="#"><i class="fa-regular fa-envelope"></i></a>
-                <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
-                <a href="https://www.facebook.com/InvasionPlus" target="_blank" title="Facebook">
-                    <i class="fa-brands fa-facebook"></i>
-                </a>
-                <a href="https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Finvasionplus%3Figsh%3DMWdheTRocjluNmN6Yw%253D%253D&is_from_rle" target="_blank" title="Instagram">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-                <a href="https://www.tiktok.com/@invasionplus" target="_blank" title="TikTok">
-                    <i class="fa-brands fa-tiktok"></i>
-                </a>
+
+    <div class="footer-logo-text">ENGINEERED FOR PERFORMANCE</div>
+        <div class="footer-content">
+            <div class="footer-links-group">
+                <div class="footer-column">
+                    <h3>
+                        EXPLORE
+                    </h3>
+
+                    <ul>
+                        <li>
+                            <a href="allproduct.html">
+                                All Product
+                            </a>
+                        </li>
+                        <li>
+                            <a href="cases.html">
+                                Cases
+                            </a>
+                        </li>
+                        <li>
+                            <a href="cooling.html">
+                                Cooling
+                            </a>
+                        </li>
+                        <li>
+                            <a href="monitor.html">
+                                Monitors
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="footer-column">
+                    <h3>
+                        ABOUT US
+                    </h3>
+
+                    <ul>
+                        <li>
+                            <a href="ContactUs.html">
+                                Contact Us
+                            </a>
+                        </li>
+                        <li>
+                            <a href="Location.html">
+                                Location
+                            </a>
+                        </li>
+                        <li>
+                            <a href="AboutUs.html">
+                                About Us
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="footer-column">
+                    <h3>
+                        SOFTWARE
+                    </h3>
+
+                    <ul>
+                        <li>
+                            <a href="ContactUs.html">
+                                Software 1
+                            </a>
+                        </li>
+                        <li>
+                            <a href="Location.html">
+                                Software 1
+                            </a>
+                        </li>
+                        <li>
+                            <a href="AboutUs.html">
+                                Software 1
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                
+                <div class="footer-social-section">
+                    <h3>FOLLOW US</h3>
+                    <div class="social-icons">
+                        <a href="#"><i class="fa-regular fa-envelope"></i></a>
+                        <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+                        <a href="https://www.facebook.com/InvasionPlus" target="_blank">
+                            <i class="fa-brands fa-facebook"></i>
+                        </a>
+                        <a href="https://www.instagram.com/invasionplus" target="_blank">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                        <a href="https://www.tiktok.com/@invasionplus" target="_blank">
+                            <i class="fa-brands fa-tiktok"></i>
+                        </a>
+                    </div>
+                </div>
             </div>
+
+            <div class="footer-column footer-video">
+                <h3>FEATURED CONTENT</h3>
+
+                <div class="video-embed">
+                    <iframe 
+                        src="https://www.youtube.com/embed/_69uSha2mes"
+                        title="Invasion Product Showcase"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen>
+                    </iframe>
+                </div>
+
+                <div class="video-meta">
+                    <span>LATEST VIDEO</span>
+                    <h4>Invasion Product Showcase</h4>
+                    <p>Latest builds, performance & innovations.</p>
+                </div>
+
         </div>
-    </div>
+
+
 </footer>`;
 
 // This injects the footer at the very end of the body
